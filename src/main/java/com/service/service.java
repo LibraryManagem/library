e3a5c990fc941c6e0c8ad78e55e57ca483005798
aria001 <yfyhjh@gmail.com>
2019-11-15 18:56:19 +0800

    修改测试文件

--- src/main/java/com/service/service.java
+++ src/main/java/com/service/service.java
@@ -1,11 +1,14 @@
 package com.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.dao.LendDao;
-
+@Service
 public class service {
-
+@Autowired
 	private LendDao lendao;
-	public void set(LendDao lendao) {
+	public void setLenDao(LendDao lendao) {
 		this.lendao=lendao;
 	}
 	
--- src/main/java/com/service/test.java
+++ src/main/java/com/service/test.java
@@ -1,5 +1,7 @@
 package com.service;
 
+import java.util.Arrays;
+
 import javax.swing.text.AbstractDocument.Content;
 
 import org.junit.Test;
@@ -9,7 +11,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class test {
 @Test
 	public void jdbcTest() {
-		ApplicationContext ac=new ClassPathXmlApplicationContext("resources/LibraryContext.xml");
+		ApplicationContext ac=new ClassPathXmlApplicationContext("LibraryContext.xml");
+		System.out.println("Bean names: " + Arrays.toString(ac.getBeanNamesForType(service.class)));
 	service ser=(service)ac.getBean("service");
 	ser.ad();
 	}
